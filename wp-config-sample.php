5f542a07c22357362da6bcc7ea41e1de8a5fb8cb
Jascha <jascha@jaeh.at>
2014-07-13 18:39:10 +0200

    added multisite settings

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -112,13 +112,14 @@ define( 'UPLOADS', 'static' );
 /**
  * Enables the wordpress multisite features
  */
+ 
 //define('WP_ALLOW_MULTISITE', true);
- define('MULTISITE', true);
- define('SUBDOMAIN_INSTALL', true);
- define('DOMAIN_CURRENT_SITE', '|site_url|');
- define('PATH_CURRENT_SITE', '/');
- define('SITE_ID_CURRENT_SITE', 1);
- define('BLOG_ID_CURRENT_SITE', 1);
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', true);
+define('DOMAIN_CURRENT_SITE', '|site_url|');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
 
 /* That's all, stop editing! Happy blogging. */
 
