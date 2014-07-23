77baaa7f78be1b4677cad38bc1fb9dab43295f67
Jascha <jascha@jaeh.at>
2014-07-23 18:36:08 +0200

    added bp-custom.php, updated submodules

---
+++ c/lib/bp-custom.php
@@ -0,0 +1,11 @@
+<?php
+define ( 'BP_AVATAR_THUMB_WIDTH', 50 );
+define ( 'BP_AVATAR_THUMB_HEIGHT', 50 );
+define ( 'BP_AVATAR_FULL_WIDTH', 300 );
+define ( 'BP_AVATAR_FULL_HEIGHT', 300 );
+
+define ( 'BP_AVATAR_DEFAULT', 'http://test.com/default-avatar.jpg' );
+define ( 'BP_AVATAR_DEFAULT_THUMB', 'http://test.com/default-avatar-thumb.jpg' );
+
+define ( 'BP_IGNORE_DEPRECATED', true );
+define ( 'BP_ENABLE_ROOT_PROFILES', true );
