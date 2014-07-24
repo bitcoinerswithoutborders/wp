3f6d6e81d93456327035f23babd7fbbb2ca7eb79
Jascha <jascha@jaeh.at>
2014-07-24 03:29:49 +0200

    let the sunshine in

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -114,6 +114,9 @@ define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
 
+/* Let the sun shine in */
+define('SUNRISE', 'on');
+
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
